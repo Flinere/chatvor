174c51b04826f76e84e992491958926e67d10526
sekibanki <qweh15@mail.ru>
2026-05-31 17:59:48 +0300

    Initial commit

---
+++ chatvor/chat.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace chatvor;
+
+public partial class chat : Window
+{
+    public chat()
+    {
+        InitializeComponent();
+    }
+}
